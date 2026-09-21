b7e7af21fe3fbed22fd18d5ccbef14fd499157dd
jy <jy@TAICHI>
2026-09-21 16:51:47 +0800

    Add project files.

---
+++ OpenAIMultiProviderDemo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using OpenAI.Chat;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpenAIMultiProviderDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+            ChatCompletion completion = await client.CompleteChatAsync("Say 'this is a test.'");
+
+            Debug.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+        }
+    }
+}
---
+++ OpenAIMultiProviderShared/MultiProvider.cs
@@ -0,0 +1,47 @@
+﻿using OpenAI;
+using System;
+using System.ClientModel;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAIMultiProviderShared
+{
+    public  class MultiProvider
+    {
+
+        #region Constants
+
+
+        public static string GEMINI_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/openai/";
+        public static string AZURE_ENDPOINT = "https://<resource>.openai.azure.com/openai/v1/";
+
+
+        #endregion
+
+
+
+        public static OpenAIClient GetOpenAIClient(string apiKeyIn)
+        {
+
+            ApiKeyCredential cred = new ApiKeyCredential(apiKeyIn);
+
+
+
+            OpenAIClientOptions options = new OpenAIClientOptions
+            {
+#if GEMINIOPENAI
+               Endpoint = new Uri(GEMINI_ENDPOINT)
+#elif AZUREOPENAI
+               Endpoint = new Uri(AZURE_ENDPOINT)
+#endif
+            };
+
+            return new OpenAIClient(cred, options);
+
+
+
+
+
+        }
+    }
+}
